fa2fe73ad1f12b9e1c4dc4fb1710e5045cfe5298
loveymelon <manciepmc@gmail.com>
2024-07-23 15:56:04 +0900

    Initial Commit

---
+++ PlaygroundSpace/PlaygroundSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSpaceApp.swift
+//  PlaygroundSpace
+//
+//  Created by 김진수 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
