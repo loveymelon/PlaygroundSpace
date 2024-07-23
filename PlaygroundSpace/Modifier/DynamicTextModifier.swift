6cffda6ec257621093ec1cb94980b915b80f3ccb
loveymelon <manciepmc@gmail.com>
2024-07-24 02:42:49 +0900

    Create DynamicTextModifier

--- PlaygroundSpace/Modifier/DynamicTextModifier.swift
+++ PlaygroundSpace/Modifier/DynamicTextModifier.swift
@@ -5,4 +5,27 @@
 //  Created by 김진수 on 7/23/24.
 //
 
-import Foundation
+import SwiftUI
+
+private struct DynamicTextModifier: ViewModifier {
+    let type: PSTypography
+    let font: UIFont
+    
+    func body(content: Content) -> some View {
+        content
+            .font(Font(font))
+            .fontWeight(type.weight)
+            .lineSpacing(type.height - font.lineHeight)
+            .padding((type.height - font.lineHeight) / 2)
+    }
+    
+    static func uiFontGuide(_ fontType: PSTypography) -> UIFont {
+        UIFont.systemFont(ofSize: fontType.size)
+    }
+}
+
+extension View {
+    func setTextStyle(type: PSTypography) -> some View {
+        modifier(DynamicTextModifier(type: type, font: DynamicTextModifier.uiFontGuide(type)))
+    }
+}
