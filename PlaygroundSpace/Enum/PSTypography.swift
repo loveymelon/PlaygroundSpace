6f17d1891638384b4a201d6c618f13bd536d84df
loveymelon <manciepmc@gmail.com>
2024-07-24 02:42:03 +0900

    Create PSTypograpy Enum

--- PlaygroundSpace/Enum/PSTypography.swift
+++ PlaygroundSpace/Enum/PSTypography.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum PSTypography {
+    case title1
+    case title2
+    case bodyBold
+    case body
+    case caption
+    
+    var size: CGFloat {
+        switch self {
+        case .title1:
+            return 22
+        case .title2:
+            return 14
+        case .bodyBold, .body:
+            return 13
+        case .caption:
+            return 12
+        }
+    }
+    
+    var height: CGFloat {
+        switch self {
+        case .title1:
+            return 30
+        case .title2:
+            return 20
+        case .bodyBold, .body, .caption:
+            return 18
+        }
+    }
+    
+    var weight: Font.Weight {
+        switch self {
+        case .title1, .title2, .bodyBold:
+            return .bold
+        case .body, .caption:
+            return .regular
+        }
+    }
+    
+}
