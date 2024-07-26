79dc471aef5596a6f17eb62ae661436f229789d0
loveymelon <manciepmc@gmail.com>
2024-07-26 21:08:46 +0900

    Update InfoText For SignUpView

--- PlaygroundSpace/Enum/InfoText.swift
+++ PlaygroundSpace/Enum/InfoText.swift
@@ -9,4 +9,9 @@ import Foundation
 
 enum InfoText {
     static let splash = "새싹톡을 사용하면 어디서나\n팀을 모을 수 있습니다."
+    static let start = "시작하기"
+    static let or = "또는"
+    static let signUp = "또는 새롭게 회원가입 하기"
+    static let duplicate = "중복 확인"
+    static let signUpEnter = "가입하기"
 }
