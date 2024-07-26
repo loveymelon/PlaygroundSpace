5f3cbe00928b9f0eea15320fad6f330daf239eba
loveymelon <manciepmc@gmail.com>
2024-07-26 21:12:45 +0900

    Setting Coordinator Rducer

--- PlaygroundSpace/Scene/Coordinator.swift
+++ PlaygroundSpace/Scene/Coordinator.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import ComposableArchitecture
+import FlowStacks
+import TCACoordinators
+
+//@Reducer(state: .equatable)
+//enum Screen {
+//    case splash(SplashView)
+//    case onboarding(OnboardingView)
+//}
+//
+//@Reducer
+//struct Coordinator {
+//    @ObservableState
+//      struct State: Equatable {
+//          var routes: [Route<Screen.State>]
+//      }
+//    
+//    enum Action {
+//        case router(IndexedRouterActionOf<Screen>)
+//      }
+//    
+//    var body: some ReducerOf<Self> {
+//        Reduce<State, Action> { state, action in
+//            switch action {
+//                //            case .router(.routeAction(id: _, action: .)):
+//                //                <#code#>
+//                //            }
+//            default:
+//                break
+//            }
+//            return .none
+//        }
+//    }
+//}
