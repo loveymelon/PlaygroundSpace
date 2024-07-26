b1d4a831bc1ee9545f6b09ceb0901d414c4d338b
loveymelon <manciepmc@gmail.com>
2024-07-26 21:09:47 +0900

    Create ColoredSubStringModifier

--- PlaygroundSpace/Modifier/ColoredSubstringModifier.swift
+++ PlaygroundSpace/Modifier/ColoredSubstringModifier.swift
@@ -5,4 +5,34 @@
 //  Created by 김진수 on 7/26/24.
 //
 
-import Foundation
+import SwiftUI
+
+private struct ColoredSubstringModifier: ViewModifier {
+    let originalText: String
+    let coloredSubstring: String
+    let color: Color
+    
+    func body(content: Content) -> some View {
+        if let coloredRange = originalText.range(of: coloredSubstring) {
+            let beforeRange = originalText[..<coloredRange.lowerBound]
+            let coloredText = originalText[coloredRange]
+            let afterRange = originalText[coloredRange.upperBound...]
+            
+            return Text(beforeRange)
+                .foregroundColor(.brGreen)
+            + Text(coloredText)
+                .foregroundColor(color)
+            + Text(afterRange)
+                .foregroundColor(.brGreen)
+        } else {
+            return Text(originalText)
+                .foregroundColor(.brGreen)
+        }
+    }
+}
+
+extension View {
+    func TextWithColoredSubstring(originalText: String, coloredSubstring: String, color: Color) -> some View {
+        modifier(ColoredSubstringModifier(originalText: originalText, coloredSubstring: coloredSubstring, color: color))
+    }
+}
