4fa05e9cedaf5cdacb1598d36dc4455eca6ec3eb
loveymelon <manciepmc@gmail.com>
2024-07-26 21:14:24 +0900

    Create SignUpView UI

--- PlaygroundSpace/Scene/SignUp/SignUpView.swift
+++ PlaygroundSpace/Scene/SignUp/SignUpView.swift
@@ -8,11 +8,91 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var text: String = ""
+    @State var numberText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            Color(.baPrimary)
+            
+            makeSignUpView()
+            
+            makeButton(text: InfoText.signUpEnter)
+                .frame(height: 68)
+                .padding(.horizontal, 25)
+        }
     }
 }
 
-#Preview {
-    SignUpView()
+extension SignUpView {
+    private func makeSignUpView() -> some View {
+        VStack(spacing: 10) {
+            ForEach(SignUp.allCases, id: \.self) { item in
+                makeLabeledTextField(type: item, secure: item.secure)
+            }
+            
+            Spacer()
+        }
+        .padding(.top, 30)
+        .padding(.horizontal, 25)
+    }
+    
+    private func makeLabeledTextField(type: SignUp, secure: Bool) -> some View {
+        VStack {
+            HStack {
+                Text(type.rawValue)
+                    .setTextStyle(type: .title2)
+                    .foregroundStyle(.brBlack)
+                
+                Spacer()
+            }
+            
+            checkDuplicateTextField(type: type, secure: secure)
+        }
+    }
+    
+    private func makeButton(text: String) -> some View {
+        Button {
+            print("tap")
+        } label: {
+            Text(text)
+                .asText(type: .title2, foreColor: .brWhite, backColor: .brGreen)
+        }
+    }
+    
+    @ViewBuilder
+    private func makeTextField(placeHolder: String, secure: Bool) -> some View {
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundStyle(.baSecondary)
+            .frame(height: 44)
+            .overlay {
+                Group {
+                    if !secure {
+                        TextField(placeHolder, text: $text)
+                    } else {
+                        SecureField(placeHolder, text: $text)
+                    }
+                }
+                .foregroundStyle(.brBlack)
+                .background(.baSecondary)
+                .textFieldStyle(.plain)
+                .padding(.horizontal, 10)
+            }
+    }
+    
+    @ViewBuilder
+    private func checkDuplicateTextField(type: SignUp, secure: Bool) -> some View {
+        
+        if type != SignUp.email {
+            makeTextField(placeHolder: type.placeHolder, secure: secure)
+        } else {
+            HStack(spacing: 10) {
+                makeTextField(placeHolder: type.placeHolder, secure: secure)
+                
+                makeButton(text: InfoText.duplicate)
+                    .frame(width: 100, height: 44)
+            }
+        }
+        
+    }
 }
