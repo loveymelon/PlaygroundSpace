3d7ebbc78a259d841e2b98484a444a9219534dd2
loveymelon <manciepmc@gmail.com>
2024-07-29 21:15:12 +0900

    Create UserRouter

--- PlaygroundSpace/Router/UserRouter.swift
+++ PlaygroundSpace/Router/UserRouter.swift
@@ -1,8 +1,66 @@
 //
-//  UserRouter.swift
+//  SignUpRouter.swift
 //  PlaygroundSpace
 //
 //  Created by 김진수 on 7/27/24.
 //
 
 import Foundation
+import Alamofire
+
+enum UserRouter: Router {
+    case duplicate(UserEmailRequestDTO)
+    case signUpEnter(UserSignUpRequestDTO)
+}
+
+extension UserRouter {
+    var method: HTTPMethod {
+        switch self {
+        case .duplicate:
+            return .post
+        case .signUpEnter:
+            return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .duplicate:
+            return APIKey.version + "users/validation/email"
+        case .signUpEnter:
+            return APIKey.version + "users/join"
+        }
+    }
+    
+    var optionalHeaders: HTTPHeaders? {
+        switch self {
+        case .duplicate, .signUpEnter:
+            return nil
+        }
+    }
+
+    var parameters: Parameters? {
+        switch self {
+        case .duplicate, .signUpEnter:
+            return nil
+        }
+    }
+    
+    var body: Data? {
+        switch self {
+        case .duplicate(let userEmailRequestDTO):
+            return requestToBody(userEmailRequestDTO)
+        case .signUpEnter(let userSignInfo):
+            return requestToBody(userSignInfo)
+        }
+    }
+    
+    var encodingType: EncodingType {
+        switch self {
+        case .duplicate(_):
+            return .json
+        case .signUpEnter:
+            return .json
+        }
+    }
+}
