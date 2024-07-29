ff7dc67a1c6dafa5bad925f8b1a3d657debd29bd
loveymelon <manciepmc@gmail.com>
2024-07-29 21:17:01 +0900

    Create SignUpRepository

--- PlaygroundSpace/Scene/SignUp/Repository/SignUpRepository.swift
+++ PlaygroundSpace/Scene/SignUp/Repository/SignUpRepository.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+final class SignUpRepository {
+    
+    let mapper = SignUpMapper()
+    
+    func duplicatedEmail(email: String) {
+        let requestDTO = mapper.emailEntityToDTO(email: email)
+        
+        print("here?")
+        NetworkManager.shared.duplicateEmail(userEmailDTO: requestDTO)
+    }
+    
+    func signUpEnter(email: String, nick: String, number: String, password: String) {
+        let requestDTO = mapper.signUpInfoEntityToDTO(email: email, nick: nick, number: number, password: password)
+        
+        NetworkManager.shared.signUpFinish(userInfo: requestDTO)
+    }
+}
