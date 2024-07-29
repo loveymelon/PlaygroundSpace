421cb111fdd7d82d3a63b0f46a7a497db26a7539
loveymelon <manciepmc@gmail.com>
2024-07-29 21:16:43 +0900

    Create SignUpMapper

--- PlaygroundSpace/Scene/SignUp/Mapper/SignUpMapper.swift
+++ PlaygroundSpace/Scene/SignUp/Mapper/SignUpMapper.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+final class SignUpMapper {
+    
+    func emailEntityToDTO(email: String) -> UserEmailRequestDTO {
+        return UserEmailRequestDTO(email: email)
+    }
+    
+    func signUpInfoEntityToDTO(email: String, nick: String, number: String, password: String) -> UserSignUpRequestDTO {
+        return UserSignUpRequestDTO(email: email, password: password, nickname: nick, phone: number)
+    }
+}
