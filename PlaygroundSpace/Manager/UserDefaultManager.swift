aab5e078ca83f5ba8768f9f52f91cb01ebf54894
loveymelon <manciepmc@gmail.com>
2024-07-29 21:37:43 +0900

    Create UserDefaultManager

---
+++ PlaygroundSpace/Manager/UserDefaultManager.swift
@@ -0,0 +1,55 @@
+//
+//  UserDefaultManager.swift
+//  PlaygroundSpace
+//
+//  Created by 김진수 on 7/29/24.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    
+    private init() {}
+    
+    let ud = UserDefaults.standard
+    
+    enum UDKey: String {
+        case userNickname
+        case accessToken
+        case refreshToken
+    }
+
+    var userNickname: String {
+        get {
+            ud.string(forKey: UDKey.userNickname.rawValue) ?? "없음"
+        }
+        set {
+            ud.set(newValue, forKey: UDKey.userNickname.rawValue)
+        }
+    }
+    
+    var accessToken: String {
+        get {
+            ud.string(forKey: UDKey.accessToken.rawValue) ?? "없음"
+        }
+        set {
+            ud.set(newValue, forKey: UDKey.accessToken.rawValue)
+        }
+    }
+    
+    var refreshToken: String {
+        get {
+            ud.string(forKey: UDKey.refreshToken.rawValue) ?? "없음"
+        }
+        set {
+            ud.set(newValue, forKey: UDKey.refreshToken.rawValue)
+        }
+    }
+    
+    func deleteObject() {
+        ud.removeObject(forKey: UDKey.userNickname.rawValue)
+        ud.removeObject(forKey: UDKey.accessToken.rawValue)
+        ud.removeObject(forKey: UDKey.refreshToken.rawValue)
+    }
+}
