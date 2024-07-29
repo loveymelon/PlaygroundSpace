193f9fc5387448b12ab2f3ba24034f23e0546636
loveymelon <manciepmc@gmail.com>
2024-07-29 21:17:33 +0900

    Update SignUpFeature (중복 확인 기능 추가, 가입하기 기능 추가)

--- PlaygroundSpace/Scene/SignUp/Feature/SignUpFeature.swift
+++ PlaygroundSpace/Scene/SignUp/Feature/SignUpFeature.swift
@@ -20,18 +20,36 @@ struct SignUpFeature {
         
         var emailEdit: Bool = false
         var requiredIsValid: Bool = false
+        
+        var completeViewState: CompleteFeature.State?
     }
     
     enum Action: BindableAction {
         case binding(BindingAction<State>)
         
         case backButtonTapped
+        case duplicateButtonTapped
+        case signUpButtonTapped
+        case phoneRegex
+        case showCompleteView
+        
+        case completeViewAction(CompleteFeature.Action)
+        
+        case networking(NetworkType)
         
         case delegate(Delegate)
         enum Delegate {
             case signUpAction
+            case signUpFinish
         }
     }
+    
+    enum NetworkType {
+        case duplicate
+        case signUpEnter
+    }
+    
+    let repository = SignUpRepository()
     
     var body: some ReducerOf<Self> {
         BindingReducer()
@@ -49,10 +67,46 @@ struct SignUpFeature {
                 let requiredValid = (state.emailEdit && !state.nicknameText.isEmpty && !state.passwordText.isEmpty && !state.checkPasswordText.isEmpty)
                 
                 state.requiredIsValid = requiredValid
+                
+                return .run { send in
+                    await send(.phoneRegex)
+                }
+            case .phoneRegex:
+                state.phoneText = state.phoneText.formatPhoneNumber
+            case .duplicateButtonTapped:
+                return .run { send in
+                    await send(.networking(.duplicate))
+                }
+            case .signUpButtonTapped:
+                return .run { send in
+                    await send(.networking(.signUpEnter))
+                    await send(.delegate(.signUpFinish))
+                }
+            case .showCompleteView:
+                state.completeViewState = CompleteFeature.State()
+                
+            case .networking(.duplicate):
+                return .run { [emailText = state.emailText] send in
+                    repository.duplicatedEmail(email: emailText)
+                }
+            case .networking(.signUpEnter):
+                return .run { [emailText = state.emailText, nickname = state.nicknameText, number = state.phoneText, password = state.passwordText] send in
+                    repository.signUpEnter(email: emailText, nick: nickname, number: number, password: password)
+                }
+                // networking Type을 받자
+//            case .networking(.one):
+//                
+//            case .networking(.two):
+//                
+//            case .networking(.three):
+//
             default:
                 break
             }
             return .none
+        }
+        .ifLet(\.completeViewState, action: \.completeViewAction) {
+            CompleteFeature()
         }
     }
 }
