d66561aa269d1cd86335e8d82fec704ca005d476
loveymelon <manciepmc@gmail.com>
2024-07-30 23:58:03 +0900

    Create HomeEmptyView

--- PlaygroundSpace/Scene/HomeEmpty/View/HomeEmptyView.swift
+++ PlaygroundSpace/Scene/HomeEmpty/View/HomeEmptyView.swift
@@ -6,13 +6,58 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct HomeEmptyView: View {
+    @Perception.Bindable var store: StoreOf<HomeEmptyFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithPerceptionTracking {
+            NavigationStack {
+                makeHomeEmptyView()
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Image(ImageNames.workSpaceDefaultImage)
+                            makeText(text: store.viewTextState.title, type: .title1)
+                        }
+                    }
+            }
+        }
     }
 }
 
-#Preview {
-    HomeEmptyView()
+extension HomeEmptyView {
+    func makeHomeEmptyView() -> some View {
+        VStack {
+            makeText(text: store.viewTextState.mainText, type: .title1)
+            Spacer()
+            makeText(text: store.viewTextState.detailText, type: .bodyBold)
+            makeImage()
+            Spacer()
+            makeButton()
+        }
+    }
+    
+    func makeText(text: String, type: PSTypography) -> some View {
+        Text(text)
+            .setTextStyle(type: type)
+            .multilineTextAlignment(.center)
+    }
+    
+    func makeImage() -> some View {
+        Image(ImageNames.homeEmptyImage)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.horizontal, 10)
+            .clipped()
+    }
+    
+    func makeButton() -> some View {
+        Button {
+            print("create")
+        } label: {
+            Text(store.viewTextState.createText)
+                .asText(type: .title2, foreColor: .brWhite, backColor: .brGreen)
+        }
+    }
 }
