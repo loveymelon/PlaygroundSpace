79517dfdf9fe3d0be30862fab57b4ef7859b8182
loveymelon <manciepmc@gmail.com>
2024-07-30 23:53:32 +0900

    Update SignUpMapper (DTO를 Entity로 변경해주는 로직)

--- PlaygroundSpace/Scene/SignUp/Mapper/SignUpMapper.swift
+++ PlaygroundSpace/Scene/SignUp/Mapper/SignUpMapper.swift
@@ -16,4 +16,8 @@ final class SignUpMapper {
     func signUpInfoEntityToDTO(email: String, nick: String, number: String, password: String) -> UserSignUpRequestDTO {
         return UserSignUpRequestDTO(email: email, password: password, nickname: nick, phone: number)
     }
+    
+    func dtotoEntity(dto: UserDTOModel) -> UserEntity {
+        return UserEntity(userId: dto.userId, email: dto.email, nickname: dto.nickname, profileImage: dto.profileImage, phone: dto.phone, provider: dto.provider, createdAt: dto.createdAt, token: TokenEntity(accessToken: dto.token.accessToken, refreshToken: dto.token.refreshToken))
+    }
 }
