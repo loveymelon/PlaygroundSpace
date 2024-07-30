facbbbb00ba738f8973812bb53dda40659215b01
loveymelon <manciepmc@gmail.com>
2024-07-30 23:51:30 +0900

    Create WorkSpaceRouter

--- PlaygroundSpace/Router/WorkspaceRouter.swift
+++ PlaygroundSpace/Router/WorkspaceRouter.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Alamofire
+
+enum WorkspaceRouter: Router {
+    case fetch
+}
+
+extension WorkspaceRouter {
+    var method: HTTPMethod {
+        switch self {
+        case .fetch:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .fetch:
+            return APIKey.version + "workspaces"
+        }
+    }
+    
+    var optionalHeaders: HTTPHeaders? {
+        switch self {
+        case .fetch:
+            return [HTTPHeader(name: "Authorization", value: UserDefaultsManager.shared.accessToken)]
+        }
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .fetch:
+            return nil
+        }
+    }
+    
+    var body: Data? {
+        switch self {
+        case .fetch:
+            return nil
+        }
+    }
+    
+    var encodingType: EncodingType {
+        switch self {
+        case .fetch:
+            return .url
+        }
+    }
+    
+}
