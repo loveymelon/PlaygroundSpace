ed91ac569c8437f7b611b69853463a78db7b8f4c
loveymelon <manciepmc@gmail.com>
2024-07-30 23:52:29 +0900

    Create UserEntity

--- PlaygroundSpace/UserEntity.swift
+++ PlaygroundSpace/UserEntity.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct UserEntity {
+    let userId: String
+    let email: String
+    let nickname: String
+    let profileImage: String?
+    let phone: String?
+    let provider: String?
+    let createdAt: String
+    let token: TokenEntity
+}
+
+struct TokenEntity {
+    let accessToken: String
+    let refreshToken: String
+}
