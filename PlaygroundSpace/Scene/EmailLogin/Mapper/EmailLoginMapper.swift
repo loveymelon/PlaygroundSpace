27ca76aed12ea5c756c26e8e727e8f64aa8c867a
loveymelon <manciepmc@gmail.com>
2024-07-30 23:55:22 +0900

    Create EmailLogin Mapper, Repository

--- PlaygroundSpace/Scene/EmailLogin/Mapper/EmailLoginMapper.swift
+++ PlaygroundSpace/Scene/EmailLogin/Mapper/EmailLoginMapper.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+final class EmailLoginMapper {
+    func entityToRequestDTO(email: String, password: String) -> EmailLoginRequestDTO {
+        return EmailLoginRequestDTO(email: email, password: password)
+    }
+    
+    func dtoToEntity(dto: UserDTOModel) -> UserEntity {
+        return UserEntity(userId: dto.userId, email: dto.email, nickname: dto.nickname, profileImage: dto.profileImage, phone: dto.phone, provider: dto.provider, createdAt: dto.createdAt, token: TokenEntity(accessToken: dto.token.accessToken, refreshToken: dto.token.refreshToken))
+    }
+}
--- PlaygroundSpace/Scene/EmailLogin/Repository/EmailLoginRepository.swift
+++ PlaygroundSpace/Scene/EmailLogin/Repository/EmailLoginRepository.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+final class EmailLoginRepository {
+    
+    let mapper = EmailLoginMapper()
+    
+    @Dependency(\.networkManager) var network
+    
+    func login(email: String, password: String) async -> UserEntity? {
+        let request = mapper.entityToRequestDTO(email: email, password: password)
+        
+        do {
+            let result = try await network.requestNetwork(dto: UserDTOModel.self, router: UserRouter.emailLogin(EmailLoginRequestDTO(email: request.email, password: request.password)))
+            
+            switch result {
+            case .success(let data):
+                let entity = mapper.dtoToEntity(dto: data)
+                return entity
+            case .failure(let failure):
+                print(failure)
+                return nil
+            }
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+}
