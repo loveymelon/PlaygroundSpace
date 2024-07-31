ad10afa20de1730556b2e1da9161e304146dcd8a
loveymelon <manciepmc@gmail.com>
2024-07-31 13:38:21 +0900

    Update Assets (Add Camera image)

---
+++ PlaygroundSpace/Scene/WorkSpaceCreate/Feature/WorkSpaceCreateFeature.swift
@@ -0,0 +1,8 @@
+//
+//  WorkSpaceCreateFeature.swift
+//  PlaygroundSpace
+//
+//  Created by 김진수 on 7/31/24.
+//
+
+import Foundation
---
+++ PlaygroundSpace/Scene/WorkSpaceCreate/View/WorkSpaceCreateView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkSpaceCreateView.swift
+//  PlaygroundSpace
+//
+//  Created by 김진수 on 7/31/24.
+//
+
+import SwiftUI
+
+struct WorkSpaceCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkSpaceCreateView()
+}
