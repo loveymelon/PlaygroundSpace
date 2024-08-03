e857b9f3704093a684876a4ef862a7cf6388c31d
loveymelon <manciepmc@gmail.com>
2024-08-03 15:21:41 +0900

    Update WorkSpaceRouter (워크스페이스 생성시 router 추가)

--- PlaygroundSpace/Router/WorkspaceRouter.swift
+++ PlaygroundSpace/Router/WorkspaceRouter.swift
@@ -10,6 +10,7 @@ import Alamofire
 
 enum WorkspaceRouter: Router {
     case fetch
+    case create(String, String, Data)
 }
 
 extension WorkspaceRouter {
@@ -17,33 +18,35 @@ extension WorkspaceRouter {
         switch self {
         case .fetch:
             return .get
+        case .create:
+            return .post
         }
     }
     
     var path: String {
         switch self {
-        case .fetch:
-            return APIKey.version + "workspaces"
+        case .fetch, .create:
+            return APIKey.version + "/workspaces"
         }
     }
     
     var optionalHeaders: HTTPHeaders? {
         switch self {
-        case .fetch:
+        case .fetch, .create:
             return [HTTPHeader(name: "Authorization", value: UserDefaultsManager.shared.accessToken)]
         }
     }
     
     var parameters: Parameters? {
         switch self {
-        case .fetch:
+        case .fetch, .create:
             return nil
         }
     }
     
     var body: Data? {
         switch self {
-        case .fetch:
+        case .fetch, .create:
             return nil
         }
     }
@@ -52,6 +55,14 @@ extension WorkspaceRouter {
         switch self {
         case .fetch:
             return .url
+        case let .create(name, description, imageData):
+            let data = MultipartFormData()
+            
+            data.append(name.data(using: .utf8)!, withName: "name")
+            data.append(description.data(using: .utf8)!, withName: "description")
+            data.append(imageData, withName: "image", fileName: "workSpaceImage.jpeg", mimeType: "image/jpeg")
+            
+            return .multiPart(data)
         }
     }
     
