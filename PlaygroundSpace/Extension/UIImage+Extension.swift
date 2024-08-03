d700be1ba698b08bf5cc7e92ed99fb2e3296c43a
loveymelon <manciepmc@gmail.com>
2024-08-03 15:10:09 +0900

    Create imageZipLimit (이미지 압축 메소드)

--- PlaygroundSpace/Extension/UIImage+Extension.swift
+++ PlaygroundSpace/Extension/UIImage+Extension.swift
@@ -5,4 +5,27 @@
 //  Created by 김진수 on 8/2/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func imageZipLimit(zipRate: Double) -> Data? {
+        let limitBytes = zipRate * 1024 * 1024
+        var currentQuality: CGFloat = 0.7
+        var imageData = self.jpegData(compressionQuality: currentQuality)
+        
+        while let data = imageData,
+              Double(imageData!.count) > limitBytes && currentQuality > 0 {
+            currentQuality -= 0.1
+            imageData = self.jpegData(compressionQuality: currentQuality)
+        }
+        
+        if let data = imageData,
+           Double(data.count) <= limitBytes {
+            return data
+        } else {
+            return nil
+        }
+    }
+    
+}
