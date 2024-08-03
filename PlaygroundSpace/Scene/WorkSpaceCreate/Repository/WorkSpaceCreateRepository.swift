7d18736c3d1f4d8311634ea32afa361850a0efed
loveymelon <manciepmc@gmail.com>
2024-08-03 15:24:01 +0900

    Create WorkSpaceCreateRepository

--- PlaygroundSpace/Scene/WorkSpaceCreate/Repository/WorkSpaceCreateRepository.swift
+++ PlaygroundSpace/Scene/WorkSpaceCreate/Repository/WorkSpaceCreateRepository.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct WorkSpaceCreateRepository {
+    
+    @Dependency(\.networkManager) var network
+    
+    func workSpaceCreateFinish(name: String, description: String, imageData: Data) async -> Void? {
+        print(imageData.count)
+        
+        do {
+            let result = try await network.requestNetwork(dto: WorkSpaceInfoDTO.self, router: WorkspaceRouter.create(name, description, imageData))
+            
+            switch result {
+            case .success(let data):
+                print(data)
+                return ()
+            case .failure(let error):
+                print(error)
+                return nil
+            }
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
