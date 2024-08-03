cbe5654e249cbd20db3e48c36f4ef5e96137ebaa
loveymelon <manciepmc@gmail.com>
2024-08-03 15:23:06 +0900

    Create ImagePicker (유저의 앨범 접근)

--- PlaygroundSpace/Scene/Custom/ImagePicker.swift
+++ PlaygroundSpace/Scene/Custom/ImagePicker.swift
@@ -5,4 +5,44 @@
 //  Created by 김진수 on 8/2/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    var imageData: (Data) -> Void
+    @Environment(\.presentationMode) var mode
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+extension ImagePicker {
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else {
+                parent.mode.wrappedValue.dismiss()
+                return
+            }
+            
+            parent.imageData(image.imageZipLimit(zipRate: 1)!)
+            parent.mode.wrappedValue.dismiss()
+        }
+    }
+}
