b45ac95f8f864639f9fff1f875bfca7fdd2fa4a2
loveymelon <manciepmc@gmail.com>
2024-08-03 15:25:39 +0900

    Create HomeInitRepository (network 통신담당)

--- PlaygroundSpace/Scene/HomeInit/Repository/HomeInitRepository.swift
+++ PlaygroundSpace/Scene/HomeInit/Repository/HomeInitRepository.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct HomeInitRepository {
+    
+    private let mapper = HomeInitMapper()
+    
+    @Dependency(\.networkManager) var network
+    
+    func fetchChannel(workSpaceId: String) async -> ChannelListEntity? {
+        print("work", workSpaceId)
+        do {
+            let result = try await network.requestNetwork(dto: ChannelListDTO.self, router: ChannelRouter.fetchMyChannel(workSpaceId))
+            
+            switch result {
+            case .success(let data):
+                print("success", data)
+                return mapper.dtoToEntity(data)
+            case .failure(let error):
+                print("error", error)
+                return nil
+            }
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+    
+}
