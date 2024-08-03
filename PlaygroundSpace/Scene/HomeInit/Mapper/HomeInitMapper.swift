3c55a8cab50451e48747166fc11ce1af26d5a638
loveymelon <manciepmc@gmail.com>
2024-08-03 15:25:04 +0900

    Create HomeInitMapper (DTO, Entity 변환 담당)

--- PlaygroundSpace/Scene/HomeInit/Mapper/HomeInitMapper.swift
+++ PlaygroundSpace/Scene/HomeInit/Mapper/HomeInitMapper.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+final class HomeInitMapper {
+    
+    func dtoToEntity(channelDTO: ChannelDTO) -> ChannelEntity {
+        return ChannelEntity(channelId: channelDTO.channelId, name: channelDTO.name, description: channelDTO.description, coverImage: channelDTO.coverImage, ownerId: channelDTO.ownerId, createdAt: channelDTO.createdAt)
+    }
+    
+    func dtoToEntity(_ channelListDTO: ChannelListDTO) -> ChannelListEntity {
+        return ChannelListEntity(channelList: channelListDTO.channelList.map { dtoToEntity(channelDTO: $0) })
+    }
+    
+}
