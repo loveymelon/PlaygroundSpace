95f2921955e71e214e3f6a27e7d8c9cb87677753
loveymelon <manciepmc@gmail.com>
2024-08-06 21:59:05 +0900

    Update HomeInitRepository (fetchDMS)

--- PlaygroundSpace/Sources/Scene/HomeInit/Repository/HomeInitRepository.swift
+++ PlaygroundSpace/Sources/Scene/HomeInit/Repository/HomeInitRepository.swift
@@ -14,14 +14,16 @@ struct HomeInitRepository {
     
     @Dependency(\.networkManager) var network
     
-    func fetchChannel(workSpaceId: String) async -> ChannelListEntity? {
-        print("work", workSpaceId)
+}
+
+extension HomeInitRepository {
+    /// fetchChannel
+    func fetchData(workSpaceId: String) async -> ChannelListEntity? {
         do {
             let result = try await network.requestNetwork(dto: ChannelListDTO.self, router: ChannelRouter.fetchMyChannel(workSpaceId))
             
             switch result {
             case .success(let data):
-                print("success", data)
                 return mapper.dtoToEntity(data)
             case .failure(let error):
                 print("error", error)
@@ -33,4 +35,21 @@ struct HomeInitRepository {
         }
     }
     
+    /// fetchDMS
+    func fetchData() async -> DMListEntity? {
+        do {
+            let result = try await network.requestNetwork(dto: DMListDTO.self, router: DMSRouter.fetchDMList)
+            
+            switch result {
+            case .success(let data):
+                return mapper.dtoToEntity(data)
+            case .failure(let error):
+                print(error)
+                return nil
+            }
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
 }
