f4af818ddc4eee92da334ba3142662322c829770
loveymelon <manciepmc@gmail.com>
2024-08-06 21:50:42 +0900

    Create AuthRouter (Token)

--- PlaygroundSpace/Sources/Network/Router/AuthRouter.swift
+++ PlaygroundSpace/Sources/Network/Router/AuthRouter.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import Alamofire
+
+enum AuthRouter: Router {
+    case token
+}
+
+extension AuthRouter {
+    var method: HTTPMethod {
+        switch self {
+        case .token:
+        return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .token:
+            return APIKey.version + "/auth/" + "refresh"
+        }
+    }
+    
+    var optionalHeaders: HTTPHeaders? {
+        switch self {
+        case .token:
+            return [HTTPHeader(name: "Authorization", value: UserDefaultsManager.shared.accessToken)]
+        }
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .token:
+            return nil
+        }
+    }
+    
+    var body: Data? {
+        switch self {
+        case .token:
+            return nil
+        }
+    }
+    
+    var encodingType: EncodingType {
+        switch self {
+        case .token:
+            return .url
+        }
+    }
+}
