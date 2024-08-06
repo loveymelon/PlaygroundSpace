9b19805aa7c75f237b09556e468eecd14ce4d096
loveymelon <manciepmc@gmail.com>
2024-08-06 21:58:25 +0900

    Update HomeInitMapper ( DMSDTO -> DMSEntity, DMListDTO -> DMListEntity )

--- PlaygroundSpace/Sources/Scene/HomeInit/Mapper/HomeInitMapper.swift
+++ PlaygroundSpace/Sources/Scene/HomeInit/Mapper/HomeInitMapper.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class HomeInitMapper {
+struct HomeInitMapper {
     
     func dtoToEntity(channelDTO: ChannelDTO) -> ChannelEntity {
         return ChannelEntity(channelId: channelDTO.channelId, name: channelDTO.name, description: channelDTO.description, coverImage: channelDTO.coverImage, ownerId: channelDTO.ownerId, createdAt: channelDTO.createdAt)
@@ -17,4 +17,18 @@ final class HomeInitMapper {
         return ChannelListEntity(channelList: channelListDTO.channelList.map { dtoToEntity(channelDTO: $0) })
     }
     
+    /// MemberInfoDTO -> MemberInfoEntity
+    func dtoToEntity(_ memberInfoDTO: MemberInfoDTO) -> MemberInfoEntity {
+        return MemberInfoEntity(userId: memberInfoDTO.userId, email: memberInfoDTO.email, nickname: memberInfoDTO.nickname, profileImage: memberInfoDTO.profileImage)
+    }
+    
+    /// DMSDTO -> DMSEntity
+    func dtoToEntity(_ dmsDTO: DMSDTO) -> DMSEntity {
+        return DMSEntity(roomId: dmsDTO.roomId, createdAt: dmsDTO.createdAt, user: dtoToEntity(dmsDTO.user))
+    }
+    
+    /// DMListDTO -> DMListEntity
+    func dtoToEntity(_ dmListDTO: DMListDTO) -> DMListEntity {
+        return DMListEntity(dmlist: dmListDTO.dmListDTO.map { dtoToEntity($0) } )
+    }
 }
