adefc3ab39e9a3d980efc02aec4194d4696a7fc3
loveymelon <manciepmc@gmail.com>
2024-08-10 03:01:07 +0900

    Create DateManager (싱글톤으로 각 뷰에 맞게 날짜 대응 가능하도록 설계)

--- PlaygroundSpace/Sources/Manager/DateManager.swift
+++ PlaygroundSpace/Sources/Manager/DateManager.swift
@@ -6,3 +6,85 @@
 //
 
 import Foundation
+
+final class DateManager {
+    
+    private init() {}
+    static let shared = DateManager()
+    
+    private let isoDateFormatter = ISO8601DateFormatter()
+    private let dateFormatter = DateFormatter()
+    private let locale = Locale(identifier:"ko_KR")
+    
+    enum dateFormatType: String {
+        case slimYDM = "yy. MM. dd"
+        case fullType = "yyyy년 M월 d일"
+        case roomListType = "a hh:mm"
+        case rightChatType = "a hh시 mm분"
+        case leftChatType = "hh시 mm분 a"
+        
+        var format: String { return self.rawValue}
+    }
+    
+    
+    func toDateISO(_ dateString: String) -> Date? {
+        var calendar = Calendar.current
+        
+        isoDateFormatter.formatOptions = [
+            .withInternetDateTime,
+            .withFractionalSeconds
+        ]
+        calendar.timeZone = .current
+        
+        return isoDateFormatter.date(from: dateString)
+    }
+    
+    func toDateISO(_ date: Date) -> String {
+        
+        isoDateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        
+        return isoDateFormatter.string(from: date)
+    }
+    
+    func asDateToString(_ date: Date?) -> String {
+        guard let date else { return "" }
+        
+        dateFormatter.dateFormat = dateFormatType.slimYDM.format
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    func dateToStringToChat(_ date: Date, isMe: Bool) -> String {
+        if isMe {
+            dateFormatter.dateFormat = dateFormatType.rightChatType.format
+        } else {
+            dateFormatter.dateFormat = dateFormatType.leftChatType.format
+        }
+        
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    func dateToStringToChatSection(_ date: Date) -> String {
+        let format = dateFormatType.fullType.format
+        dateFormatter.setLocalizedDateFormatFromTemplate(format)
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    func dateToStringToRoomList(_ date: Date) -> String {
+    
+        let calendar = Calendar.current
+        
+        if calendar.isDateInToday(date) {
+            dateFormatter.dateFormat = dateFormatType.roomListType.format
+        } else {
+            dateFormatter.dateFormat = dateFormatType.fullType.format
+        }
+        
+        return dateFormatter.string(from: date)
+    }
+    
+}
