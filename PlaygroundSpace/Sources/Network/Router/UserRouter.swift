730c24c08dc4987f57deca54e1fc5c1e53e0a166
loveymelon <manciepmc@gmail.com>
2024-08-10 03:09:03 +0900

    Update UserRouter (카카오 로그인, 애플 로그인 대응)

--- PlaygroundSpace/Sources/Network/Router/UserRouter.swift
+++ PlaygroundSpace/Sources/Network/Router/UserRouter.swift
@@ -12,16 +12,14 @@ enum UserRouter: Router {
     case duplicate(UserEmailRequestDTO)
     case signUpEnter(UserSignUpRequestDTO)
     case emailLogin(EmailLoginRequestDTO)
+    case kakaoLogin(KakaoLoginRequestDTO)
+    case appleLogin(AppleLoginRequestDTO)
 }
 
 extension UserRouter {
     var method: HTTPMethod {
         switch self {
-        case .duplicate:
-            return .post
-        case .signUpEnter:
-            return .post
-        case .emailLogin:
+        case .duplicate, .signUpEnter, .emailLogin, .kakaoLogin, .appleLogin:
             return .post
         }
     }
@@ -34,19 +32,23 @@ extension UserRouter {
             return APIKey.version + "/users/join"
         case .emailLogin:
             return APIKey.version + "/users/login"
+        case .kakaoLogin:
+            return APIKey.version + "/users/login/kakao"
+        case .appleLogin:
+            return APIKey.version + "/users/login/apple"
         }
     }
     
     var optionalHeaders: HTTPHeaders? {
         switch self {
-        case .duplicate, .signUpEnter, .emailLogin:
+        case .duplicate, .signUpEnter, .emailLogin, .kakaoLogin, .appleLogin:
             return nil
         }
     }
 
     var parameters: Parameters? {
         switch self {
-        case .duplicate, .signUpEnter, .emailLogin:
+        case .duplicate, .signUpEnter, .emailLogin, .kakaoLogin, .appleLogin:
             return nil
         }
     }
@@ -59,16 +61,16 @@ extension UserRouter {
             return requestToBody(userSignInfo)
         case .emailLogin(let emailLogin):
             return requestToBody(emailLogin)
+        case let .kakaoLogin(token):
+            return requestToBody(token)
+        case let .appleLogin(appleInfo):
+            return requestToBody(appleInfo)
         }
     }
     
     var encodingType: EncodingType {
         switch self {
-        case .duplicate:
-            return .json
-        case .signUpEnter:
-            return .json
-        case .emailLogin:
+        case .duplicate, .signUpEnter, .emailLogin, .kakaoLogin, .appleLogin:
             return .json
         }
     }
