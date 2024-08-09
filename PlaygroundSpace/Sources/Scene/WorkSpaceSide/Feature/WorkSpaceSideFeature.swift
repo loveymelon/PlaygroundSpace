3a5933ca61e596b64ccaf2c2170caf2e2f7be1ad
loveymelon <manciepmc@gmail.com>
2024-08-10 03:19:35 +0900

    Create WorkSpaceSide View, Feature (사이드 메뉴)

--- PlaygroundSpace/Sources/Scene/WorkSpaceSide/Feature/WorkSpaceSideFeature.swift
+++ PlaygroundSpace/Sources/Scene/WorkSpaceSide/Feature/WorkSpaceSideFeature.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct WorkSpaceSideFeature {
+    @ObservableState
+    struct State {
+        var currentCase: CurrentViewCase = .loading
+        var currentModels: [WorkspaceListEntity] = []
+        var currentWorkSpaceID: String = ""
+    }
+    
+    enum Action {
+        case onAppear
+        
+        case sendToMakeWorkSpace
+        case networking
+        case networkSuccess([WorkspaceListEntity])
+        
+        case selectedModel(WorkspaceListEntity)
+        case delegate(Delegate)
+        
+        enum Delegate {
+            case selectWorkSpace(WorkspaceListEntity)
+        }
+    }
+    
+    enum CurrentViewCase {
+        case loading
+        case empty
+        case over
+    }
+    
+    private let repository = CoordinatorRepository()
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                state.currentWorkSpaceID = UserDefaultsManager.shared.currentWorkSpaceId
+                return .run { send in
+                    await send(.networking)
+                }
+
+            case .networking:
+                return .run { send in
+                    let result = await repository.fetchWorkspaceList()
+                    try await Task.sleep(for: .seconds(1))
+                    await send(.networkSuccess(result))
+                }
+                
+            case let .networkSuccess(datas):
+                state.currentModels = datas
+                state.currentCase = datas.isEmpty ? .empty : .over
+                
+            case let .selectedModel(data):
+                return .run { send in
+                    await send(.delegate(.selectWorkSpace(data)))
+                }
+            default:
+                break
+            }
+            return .none
+        }
+    }
+}
