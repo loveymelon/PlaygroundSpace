3cc0bca7681e03916201e0653e9622d7e9996818
loveymelon <manciepmc@gmail.com>
2024-08-10 03:08:36 +0900

    Update DMSRouter (채팅 푸시, 채팅목록, 채팅방 만들기 대응)

--- PlaygroundSpace/Sources/Network/Router/DMSRouter.swift
+++ PlaygroundSpace/Sources/Network/Router/DMSRouter.swift
@@ -9,49 +9,75 @@ import Foundation
 import Alamofire
 
 enum DMSRouter: Router {
-    case fetchDMList
+    case fetchDMRoomList
+    case fetchDMList(String)
+    case createDMRoom(DMRequestDTO)
+    case pushMessage(String, String, [String])
 }
 
 extension DMSRouter {
     var method: HTTPMethod {
         switch self {
-        case .fetchDMList:
-        return .get
+        case .fetchDMRoomList, .fetchDMList:
+            return .get
+        case .createDMRoom:
+            return .post
+        case .pushMessage:
+            return .post
         }
     }
     
     var path: String {
         switch self {
-        case .fetchDMList:
+        case .fetchDMRoomList:
             return APIKey.version + "/workspaces/" + "\(UserDefaultsManager.shared.currentWorkSpaceId)/" + "dms"
+        case let .fetchDMList(roomId), let .pushMessage(roomId, _, _):
+            return APIKey.version + "/workspaces/" + "\(UserDefaultsManager.shared.currentWorkSpaceId)/" + "dms/" + "\(roomId)/" + "chats"
+        case .createDMRoom:
+            return APIKey.version + "/workspaces/\(UserDefaultsManager.shared.currentWorkSpaceId)/dms"
         }
     }
     
     var optionalHeaders: HTTPHeaders? {
         switch self {
-        case .fetchDMList:
+        case .fetchDMRoomList, .fetchDMList, .createDMRoom, .pushMessage:
             return [HTTPHeader(name: "Authorization", value: UserDefaultsManager.shared.accessToken)]
         }
     }
     
     var parameters: Parameters? {
         switch self {
-        case .fetchDMList:
+        case .fetchDMRoomList, .fetchDMList, .createDMRoom, .pushMessage:
             return nil
         }
     }
     
     var body: Data? {
         switch self {
-        case .fetchDMList:
+        case .fetchDMRoomList, .fetchDMList, .pushMessage:
             return nil
+        case let .createDMRoom(userId):
+            return requestToBody(userId)
         }
     }
     
     var encodingType: EncodingType {
         switch self {
-        case .fetchDMList:
+        case .fetchDMRoomList, .fetchDMList:
             return .url
+        case .createDMRoom:
+            return .json
+        case let .pushMessage(_, content, files):
+            let data = MultipartFormData()
+            
+            data.append(content.data(using: .utf8)!, withName: "content")
+            
+            for (index, file) in files.enumerated() {
+                let fieldName = "file[\(index)]"
+                data.append(file.data(using: .utf8)!, withName: fieldName)
+            }
+            
+            return .multiPart(data)
         }
     }
 }
