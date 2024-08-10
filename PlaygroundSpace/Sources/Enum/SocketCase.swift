5cb117477af908e16f62aeefac5cf3423165d090
loveymelon <manciepmc@gmail.com>
2024-08-10 23:48:08 +0900

    Create SocketCase (채널 채팅과 개인 채팅 구분)

--- PlaygroundSpace/Sources/Enum/SocketCase.swift
+++ PlaygroundSpace/Sources/Enum/SocketCase.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum SocketCase {
+    case channelChat(channelID: String)
+    case dmsChat(roomID: String)
+    var address: String {
+        switch self {
+        case .channelChat(let channelID):
+            return "/ws-channel-\(channelID)"
+        case let .dmsChat(roomID):
+            return "/ws-dm-\(roomID)"
+        }
+    }
+    
+    var eventName: String {
+        switch self {
+        case .channelChat:
+            return "channel"
+        case .dmsChat:
+            return "dm"
+        }
+    }
+}
