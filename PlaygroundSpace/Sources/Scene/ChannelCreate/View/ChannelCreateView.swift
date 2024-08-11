43a5d39d8670aa57ef41ca3087c11db0b2d42a0a
loveymelon <manciepmc@gmail.com>
2024-08-11 21:20:51 +0900

    Update ChannelCreateView (레거시 제거)

--- PlaygroundSpace/Sources/Scene/ChannelCreate/View/ChannelCreateView.swift
+++ PlaygroundSpace/Sources/Scene/ChannelCreate/View/ChannelCreateView.swift
@@ -6,13 +6,98 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ChannelCreateView: View {
+    @Perception.Bindable var store: StoreOf<ChannelCreateFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithPerceptionTracking {
+            NavigationStack {
+                makeChannelCreateView()
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            makeCancleButton()
+                        }
+                    }
+                    .navigationTitle("채널 생성")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+        }
     }
 }
 
-#Preview {
-    ChannelCreateView()
+extension ChannelCreateView {
+    private func makeChannelCreateView() -> some View {
+        ZStack(alignment: .bottom) {
+            Color(.baPrimary)
+            
+            VStack {
+                ForEach(store.state.channelTextType, id: \.self) { item in
+                    makeLabeledTextField(type: item)
+                        .padding(.bottom, 20)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 30)
+            
+            makeButton()
+        }
+    }
+    
+    private func makeCancleButton() -> some View {
+        Button {
+            store.send(.viewEventType(.backButtonTapped))
+        } label: {
+            Image(ImageNames.backButton)
+        }
+    }
+    
+    private func makeLabeledTextField(type: ChannelCreateFeature.ChannelType) -> some View {
+        VStack {
+            HStack {
+                Text(type.rawValue)
+                    .setTextStyle(type: .title2)
+                    .foregroundStyle(.brBlack)
+                
+                Spacer()
+            }
+            
+            makeTextField(type: type)
+        }
+    }
+    
+    private func makeTextField(type: ChannelCreateFeature.ChannelType) -> some View {
+        RoundedRectangle(cornerRadius: 10)
+            .foregroundStyle(.baSecondary)
+            .frame(height: 44)
+            .overlay {
+                Group {
+                    switch type {
+                    case .channelName:
+                        TextField(type.placeholder, text: $store.state.viewTextState.channelNameText)
+                    case .channelExplain:
+                        TextField(type.placeholder, text: $store.state.viewTextState.channelExplainText)
+                    }
+                }
+                .foregroundStyle(.brBlack)
+                .background(.baSecondary)
+                .textFieldStyle(.plain)
+                .padding(.horizontal, 10)
+            }
+    }
+    
+    private func makeButton() -> some View {
+        Button {
+            store.send(.viewEventType(.completeButtonTapped))
+        } label: {
+            Text("생성")
+                .asText(type: .title2, foreColor: .brWhite, backColor: store.requiredIsValid ? .brGreen: .brInactive)
+        }
+        .disabled(!store.requiredIsValid)
+        .padding(.horizontal, 20)
+        .padding(.bottom, 10)
+    }
 }
