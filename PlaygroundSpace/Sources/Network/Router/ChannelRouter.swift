0a1259ef36fcc21927485facc71eeed836612201
loveymelon <manciepmc@gmail.com>
2024-08-11 21:26:45 +0900

    Update ChannelRouter (워크스페이스 내 모든 채널 탐색 라우터 추가)

--- PlaygroundSpace/Sources/Network/Router/ChannelRouter.swift
+++ PlaygroundSpace/Sources/Network/Router/ChannelRouter.swift
@@ -11,12 +11,13 @@ import Alamofire
 enum ChannelRouter: Router {
     case fetchMyChannel(String)
     case createChannel(String, String)
+    case fetchAllChannel
 }
 
 extension ChannelRouter {
     var method: HTTPMethod {
         switch self {
-        case .fetchMyChannel:
+        case .fetchMyChannel, .fetchAllChannel:
             return .get
         case .createChannel:
             return .post
@@ -27,35 +28,35 @@ extension ChannelRouter {
         switch self {
         case let .fetchMyChannel(workSpaceId):
             return APIKey.version + "/workspaces/" + "\(workSpaceId)/" + "my-channels"
-        case .createChannel:
+        case .createChannel, .fetchAllChannel:
             return APIKey.version + "/workspaces/\(UserDefaultsManager.shared.currentWorkSpaceId)/channels"
         }
     }
     
     var optionalHeaders: HTTPHeaders? {
         switch self {
-        case .fetchMyChannel, .createChannel:
+        case .fetchMyChannel, .createChannel, .fetchAllChannel:
             return [HTTPHeader(name: "Authorization", value: UserDefaultsManager.shared.accessToken)]
         }
     }
     
     var parameters: Parameters? {
         switch self {
-        case .fetchMyChannel, .createChannel:
+        case .fetchMyChannel, .createChannel, .fetchAllChannel:
             return nil
         }
     }
     
     var body: Data? {
         switch self {
-        case .fetchMyChannel, .createChannel:
+        case .fetchMyChannel, .createChannel, .fetchAllChannel:
             return nil
         }
     }
     
     var encodingType: EncodingType {
         switch self {
-        case .fetchMyChannel:
+        case .fetchMyChannel, .fetchAllChannel:
             return .url
         case let .createChannel(name, description):
             let data = MultipartFormData()
