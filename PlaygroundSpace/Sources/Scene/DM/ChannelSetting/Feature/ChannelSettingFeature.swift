209412e1f2348bc2e8b76e23001693d3278ec4a2
loveymelon <manciepmc@gmail.com>
2024-08-12 23:50:23 +0900

    Create ChannelSettingFeature, View

--- PlaygroundSpace/Sources/Scene/DM/ChannelSetting/Feature/ChannelSettingFeature.swift
+++ PlaygroundSpace/Sources/Scene/DM/ChannelSetting/Feature/ChannelSettingFeature.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct ChannelSettingFeature {
+    @ObservableState
+    struct State: Equatable {
+        var channelId: String
+        var channelData: PointChannelEntity
+    }
+    
+    enum Action {
+        case viewEventType(ViewEventType)
+        case dataTransType(DataTransType)
+        
+        case delegate(Delegate)
+        enum Delegate {
+            case backButtonTapped
+        }
+    }
+    
+    enum ViewEventType {
+        case onAppear
+        case backButtonTapped
+    }
+    
+    enum DataTransType {
+        case pointChannelData(PointChannelEntity)
+    }
+    
+    private let repository = ChannelSettingRepository()
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            case .viewEventType(.onAppear):
+                return .run { [state = state] send in
+                    let result = await repository.fetchPointChannel(channelId: state.channelId)
+                    
+                    guard let data = result else { return }
+                    
+                    await send(.dataTransType(.pointChannelData(data)))
+                }
+                
+            case .viewEventType(.backButtonTapped):
+                return .run { send in
+                    await send(.delegate(.backButtonTapped))
+                }
+                
+            case let .dataTransType(.pointChannelData(entity)):
+                state.channelData = entity
+                
+            default:
+                break
+            }
+            return .none
+        }
+    }
+}
--- PlaygroundSpace/Sources/Scene/DM/ChannelSetting/View/ChannelSettingView.swift
+++ PlaygroundSpace/Sources/Scene/DM/ChannelSetting/View/ChannelSettingView.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ChannelSettingView: View {
+    @Perception.Bindable var store: StoreOf<ChannelSettingFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithPerceptionTracking {
+            makeChannelSettingView()
+                .navigationTitle("채널 설정")
+                .toolbar(.hidden, for: .tabBar)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        makeCancleButton()
+                    }
+                }
+                .onAppear {
+                    store.send(.viewEventType(.onAppear))
+                }
+        }
     }
 }
 
-#Preview {
-    ChannelSettingView()
+extension ChannelSettingView {
+    private func makeChannelSettingView() -> some View {
+        VStack {
+            makeChannelTitleAndExplainView()
+        }
+    }
+    
+    private func makeChannelTitleAndExplainView() -> some View {
+        VStack {
+            Text("#\(store.channelData.name)")
+                .setTextStyle(type: .title2)
+            Text(store.channelData.description ?? "")
+                .setTextStyle(type: .body)
+        }
+    }
+    
+    private func makeCancleButton() -> some View {
+        Button {
+            store.send(.viewEventType(.backButtonTapped))
+        } label: {
+            Image(ImageNames.backButton)
+        }
+    }
+    
+    private func makeMemberView() -> some View {
+        DisclosureGroup("멤버(\(store.channelData.channelMembers.count))") {
+            ForEach(store.channelData.channelMembers, id: \.userId) { item in
+                memberProfileView(item.nickname, imageUrl: item.profileImage)
+            }
+            
+        }
+    }
+    
+    @ViewBuilder
+    private func memberProfileView(_ nick: String, imageUrl: String?) -> some View {
+        if let url = imageUrl {
+            DownSamplingImageView(url: URL(string: url), size: CGSize(width: 44, height: 44))
+                .frame(width: 50, height: 50)
+                
+        } else {
+            Image(ImageNames.profile)
+                .resizable()
+                .frame(width: 44, height: 44)
+        }
+    }
 }
