049cb8bbb5718c580d52b7d7e5fe0ec347c55992
loveymelon <manciepmc@gmail.com>
2024-08-12 23:40:34 +0900

    Update ChatDTO, Entity 재구성

--- PlaygroundSpace/ChatDTO.swift
+++ PlaygroundSpace/ChatDTO.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+struct ChatDTO: DTO {
+    let channelId: String
+    let channelName: String
+    let chatId: String
+    let content: String?
+    let createdAt: String
+    let files: [String]
+    let user: MemberInfoDTO
+    
+    enum CodingKeys: String, CodingKey {
+        case channelId = "channel_id"
+        case channelName
+        case chatId = "chat_id"
+        case content
+        case createdAt
+        case files
+        case user
+    }
+}
+
+struct ChatListDTO: DTO {
+    let chatListDTO: [ChatDTO]
+    
+    init(from decoder: any Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        var chatList = [ChatDTO] ()
+        while !container.isAtEnd {
+            let chat = try container.decode(ChatDTO.self)
+            chatList.append(chat)
+        }
+        self.chatListDTO = chatList
+    }
+}
--- PlaygroundSpace/ChatEntity.swift
+++ PlaygroundSpace/ChatEntity.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct ChatEntity: Entity {
+    let channelId: String
+    let channelName: String
+    let chatId: String
+    let content: String?
+    let createdAt: String
+    let files: [String]
+    let user: MemberInfoEntity
+    
+    init(channelId: String = "", channelName: String = "", chatId: String = "", content: String? = nil, createdAt: String = "", files: [String] = [], user: MemberInfoEntity = MemberInfoEntity()) {
+        self.channelId = channelId
+        self.channelName = channelName
+        self.chatId = chatId
+        self.content = content
+        self.createdAt = createdAt
+        self.files = files
+        self.user = user
+    }
+}
