ea5e3bcdfbc87d984b3d46b911c4a32e7ba76c0d
loveymelon <manciepmc@gmail.com>
2024-08-12 23:49:54 +0900

    Create ChannelSettingMapper, Repository

--- PlaygroundSpace/Sources/Scene/DM/ChannelSetting/Mapper/ChannelSettingMapper.swift
+++ PlaygroundSpace/Sources/Scene/DM/ChannelSetting/Mapper/ChannelSettingMapper.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct ChannelSettingMapper {
+    func dtoToEntity(_ dto: PointChannelDTO) -> PointChannelEntity {
+        return PointChannelEntity(channelId: dto.channelId, name: dto.name, description: dto.description, coverImage: dto.coverImage, ownerId: dto.ownerId, createdAt: dto.createdAt, channelMembers: dtoToEntity(dto.channelMembers.memberIfnoListDTO))
+    }
+}
+
+extension ChannelSettingMapper {
+    private func dtoToEntity(_ dto: [MemberInfoDTO]) -> [MemberInfoEntity] {
+        return dto.map { dtoToEntity($0) }
+    }
+    
+    private func dtoToEntity(_ dto: MemberInfoDTO) -> MemberInfoEntity {
+        return MemberInfoEntity(userId: dto.userId, email: dto.email, nickname: dto.nickname, profileImage: dto.profileImage)
+    }
+}
--- PlaygroundSpace/Sources/Scene/DM/ChannelSetting/Repository/ChannelSettingRepository.swift
+++ PlaygroundSpace/Sources/Scene/DM/ChannelSetting/Repository/ChannelSettingRepository.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct ChannelSettingRepository {
+    private let mapper = ChannelSettingMapper()
+    
+    @Dependency(\.networkManager) var network
+    
+    func fetchPointChannel(channelId: String) async -> PointChannelEntity? {
+        do {
+            let result = try await network.requestNetwork(dto: PointChannelDTO.self, router: ChannelRouter.fetchPointChannel(channelId))
+            
+            switch result {
+            case .success(let data):
+                return mapper.dtoToEntity(data)
+            case .failure(let error):
+                print(error.localizedDescription)
+                return nil
+            }
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
